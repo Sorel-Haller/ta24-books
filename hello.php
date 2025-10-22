031e0d288958b6df4c2bd28d3ccc24abdf4f2c6c
Sorel-Haller <hallersorel@gmail.com>
2025-10-22 14:56:25 +0300

    <input type="submit" name="action-submit" value="Saada">

--- hello.php
+++ hello.php
@@ -1,8 +1,25 @@
 <?php
 
 var_dump($_GET);
+//kommentaar /*blok*/
+$user = $GET["user"];
 
-$user = 'Sorel';
-
-echo "Hello, {$user}!<br>";
+echo "Hello, {$username}!<br>";
 echo "Hello, World!\n";
+?>
+
+<!doctype html>
+<html>
+<head>
+    <title>Hello Page</title>                  
+    <meta charset="utf-8">
+</head>
+<body>  
+    <form action="./hello.php" method="get">
+
+        <label for="user">Nimi:</label>
+        <input type="text" name="username" id="user">
+        <input type="submit" name="action-submit" value="Saada">
+    </form>
+</body>
+</html>
