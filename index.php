bd56a4e241a5414f12c99f760096195a313db55f
Sorel-Haller <hallersorel@gmail.com>
2025-10-22 16:46:43 +0300

    echo $row['title']. "<br>"; //  echo "{$row['title']}<br>";

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = 'localhost';
-$db   = 'bookstore';
-$user = 'root';
-$pass = '';
+$host = 'd138177.mysql.zonevs.eu';
+$db   = 'd138177_booksdb';
+$user = 'd138177_books22';
+$pass = 'Booksmysql22';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
@@ -13,3 +13,11 @@ $options = [
     PDO::ATTR_EMULATE_PREPARES   => false,
 ];
 $pdo = new PDO($dsn, $user, $pass, $options);
+
+$stmt = $pdo->query('SELECT title FROM books');
+while ($row = $stmt->fetch())
+{
+    echo $row['title']. "<br>";
+//  echo "{$row['title']}<br>";
+
+}
