2daecf16fee73d4c5ff248daa3b67a08cd25c709
Sorel-Haller <hallersorel@gmail.com>
2025-10-24 12:51:53 +0300

    git config.php -> gitignore

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$host = 'd138177.mysql.zonevs.eu';
+$db   = 'd138177_booksdb';
+$user = 'd138177_books22';
+$pass = 'Booksmysql22';
+$charset = 'utf8mb4';
--- connection.php
+++ connection.php
@@ -1,10 +1,6 @@
 <?php
 
-$host = 'd138177.mysql.zonevs.eu';
-$db   = 'd138177_booksdb';
-$user = 'd138177_books22';
-$pass = 'Booksmysql22';
-$charset = 'utf8mb4';
+require_once('./config.php');
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
