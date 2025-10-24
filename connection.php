b1aeca98ba6eae0c431e015b7d04c4aed1cbc257
Sorel-Haller <hallersorel@gmail.com>
2025-10-24 12:49:44 +0300

    connection.php

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = 'd138177.mysql.zonevs.eu';
+$db   = 'd138177_booksdb';
+$user = 'd138177_books22';
+$pass = 'Booksmysql22';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$pdo = new PDO($dsn, $user, $pass, $options);
--- index.php
+++ index.php
@@ -1,18 +1,6 @@
 <?php
 
-$host = 'd138177.mysql.zonevs.eu';
-$db   = 'd138177_booksdb';
-$user = 'd138177_books22';
-$pass = 'Booksmysql22';
-$charset = 'utf8mb4';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-$pdo = new PDO($dsn, $user, $pass, $options);
+require_once('./connection.php');
 
 $stmt = $pdo->query('SELECT title FROM books');
 while ($row = $stmt->fetch())
