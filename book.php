7eaedc2cedf2a8c71dc83cd18a2257d35a928504
Sorel-Haller <hallersorel@gmail.com>
2025-10-24 13:44:59 +0300

    ctrl u - fetch source

--- book.php
+++ book.php
@@ -1,15 +1,34 @@
 <?php
-
 require_once('./connection.php');
 
 if (!isset($_GET['id']) || !$_GET['id']) {
     echo 'Viga: raamatut ei leitud!';
     exit();
 }
-$id = $_GET['id'];
 
-$stmt = $pdo->prepare('SELECT * FROM books WHERE id= :id');
+$id = (int)$_GET['id'];
+
+$stmt = $pdo->prepare('SELECT * FROM books WHERE id = :id');
 $stmt->execute(['id' => $id]);
 $book = $stmt->fetch();
 
-var_dump($book);
+$stmt = $pdo->prepare('SELECT first_name, last_name FROM book_authors ba LEFT JOIN authors a ON ba.author_id = a.id WHERE book_id= :book_id');
+$stmt->execute(['book_id' => $id]);
+$authors = $stmt->fetchAll();
+
+
+var_dump($authors);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= htmlspecialchars($book['title']) ?></title>
+<body>
+    <h1><?= htmlspecialchars($book['title']) ?></h1>
+    <h3></h3>
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 
 require_once('./connection.php');
 
-$stmt = $pdo->query('SELECT title FROM books');
+$stmt = $pdo->query('SELECT id, title FROM books');
+
 while ($row = $stmt->fetch())
 {
 //    echo $row['title']. "<br>";
-    echo "<a href='book.php' id='title' >{$row['title']}</a><br>";
+    echo "<a href='book.php?id={$row['id']}'>{$row['title']}</a><br>"; 
 
 }
