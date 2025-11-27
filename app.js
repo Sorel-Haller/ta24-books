c8ede40834ad8c899f8964c0d8cadb5c3e027c64
Sorel-Haller <hallersorel@gmail.com>
2025-11-27 08:17:54 +0200

    Correct search input handling and display logic
    Fix search functionality and improve book list rendering.

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
+
+const bookList = document.getElementById('book-list');
 const searchInput = document.getElementById('search');
 const bookAnchors = document.getElementsByClassName('book');
 
 searchInput.addEventListener('input', e => {
+    
+    let bookListContent = '';
 
-    console.log(seaqrchInput.value);
+    Array.from(bookAnchors).forEach( book => {
 
-    Array.from(bookAnchors).forEach(book => {
-        if (title.toLowerCase().includes(searchInput.value.toLowerCase()) ){
+        const title = book.innerText;
+        const href = book.href;
+        if ( title.toLowerCase().includes(searchInput.value.toLowerCase()) ) {
 
-        bookListContent += '<li><a class="book" href="${href}">${title}</a></li>';
+            bookListContent += `<li><a class="book" href="${href}">${title}</a></li>`;
+            
         } else {
-            bookListContent = 'hidden';
-        };
-    }); 
-});
 
+            bookListContent += `<li class="hidden"><a class="book" href="${href}">${title}</a></li>`;
+        }
+
+    });
+
+    bookList.innerHTML = bookListContent;
+});
