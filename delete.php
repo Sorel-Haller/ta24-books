3ff7b9fcbc861085a653ab255bda5ef8f1b47751
Sorel-Haller <hallersorel@gmail.com>
2025-11-27 08:22:42 +0200

    Refactor delete.php for improved error handling

--- delete.php
+++ delete.php
@@ -1,18 +1,18 @@
 <?php
 
-require_once 'connection.php';
+require_once('./connection.php');
 
 $id = $_POST['id'];
 
-if ( !$id || !isset ($_POST['action']) || $_POST['action'] != 'delete') {
-    echo 'Vigane  URL!';
+if ( !$id || !isset($_POST['action']) || $_POST['action'] != 'delete' ) {
+    echo 'Viga: vigane URL!';
     exit();
-};
-
-$stmt = $pdo->query('SELECT id, title FROM books WHERE  is_deleted = 0 ');
-$stmt->execute(['book_id' => $id]); 
-
-while ($row = $stmt->fetch()) {
-    $stmt = $pdo->prepare('DELETE FROM book_authors WHERE book_id = :book_id');
-    $stmt->execute(['book_id' => $id]);
 }
+
+$stmt = $pdo->prepare('UPDATE books SET is_deleted = 1 WHERE id = :id');
+$stmt->execute([
+    'id' => $id,
+]);
+
+header("Location: ./index.php");
+die();
